80f76fcc4ee79a9fe98cf64ddcbff79131382c5c
rav4s <ravidhruvishshah@gmail.com>
2021-02-12 20:49:37 -0600

    Add more content

---
+++ js/redirects.js
@@ -0,0 +1,11 @@
+document.getElementById("smartgaragedoor").onclick = function() {
+    location.href = "/projects/smart-garage-door-controller.html";
+};
+
+document.getElementById("repairdashboard").onclick = function() {
+    location.href = "/projects/iFixit-Repairability-Dashboard.html";
+};
+
+document.getElementById("thinkpadx220").onclick = function() {
+    location.href = "/projects/thinkpad-x220-tablet-build.html";
+};
